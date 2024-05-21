2dc9f6e85c7579d2b49d1b2dd607e4582aed560c
dzsak <sisakadam@gmail.com>
2024-05-21 12:49:26 +0200

    Routes

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
 import { About } from './components/About';
 import { NoMatch } from './components/NoMatch';
@@ -9,10 +10,30 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/*" element={<NoMatch />} />
+          <Route path="/" element={
+            <>
+              <Header />
+              <Home />
+            </>
+          } />
+          <Route path="/dashboard" element={
+            <>
+              <Header />
+              <Dashboard />
+            </>
+          } />
+          <Route path="/about" element={
+            <>
+              <Header />
+              <About />
+            </>
+          } />
+          <Route path="/*" element={
+            <>
+              <Header />
+              <NoMatch />
+            </>
+          } />
         </Routes>
       </Router>
     </>
